5d376f3f2f09c34d784d2e7e4850281ab908fd69
@Syeda Huma Jamshed <159490433+syedzadigithub@users.noreply.github.com>
2024-06-13 00:24:02 +0500

    This Is my 12 Project
    Object Oriented Program

---
+++ main.js
@@ -0,0 +1,56 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new person();
+const programStart = async (persons) => {
+    do {
+        console.log(chalk.red(`         ******************************************************  WELCOME!      ****************************************************************`));
+        const answer = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: "Whom would you like to interact? ",
+            choices: ["Staff", "student", "Exit"],
+        });
+        if (answer.select === "Staff") {
+            console.log(chalk.yellow("You approach to staff room. Please feel free to ask any question.\n"));
+        }
+        else if (answer.select === "student") {
+            let answer = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: "Enter the student's name you want to interact with: ",
+            });
+            const student = persons.students.find((value) => value.name === answer.student);
+            if (!student) {
+                const name = new Student(answer.student);
+                persons.addStudent(name);
+                console.log(chalk.bgRed(`\nHello I am ${name.name}. Nice meet you!`));
+                console.log(chalk.bgMagentaBright("\nNew student added:"));
+                console.log(chalk.bgBlue(`\nCurrent Students list:\n`));
+                console.log(persons.students);
+            }
+            else {
+                console.log(chalk.bgCyan(`\nHello I am ${student.name}. Nice to see you again.`));
+                console.log(chalk.bgGreen(`\nExisting Students list:\n`));
+                console.log(persons.students);
+            }
+        }
+        else if (answer.select === "Exit") {
+            console.log(chalk.redBright("\n Exiting the program...\t \t \t \t"));
+            process.exit();
+        }
+    } while (true);
+};
+programStart(persons);
